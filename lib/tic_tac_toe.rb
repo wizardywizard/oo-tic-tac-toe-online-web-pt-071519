bcad65d7284128fb2e15787bcf323b97f63d8421
David Little <littledavid175@gmail.com>
2019-07-29 22:33:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -83,7 +83,6 @@ class TicTacToe
   def won?
     WIN_COMBINATIONS.detect do |win|
       @board[win[0]] == @board[win[1]] && @board[win[1]] == @board[win[2]]
-     # binding.pry
     end
   end
   
