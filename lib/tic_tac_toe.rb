6ceda8c22d0b84c5adb5d567a59f848ef8fd062c
David Little <littledavid175@gmail.com>
2019-07-29 20:40:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,7 +40,7 @@ class TicTacToe
   end
   
   def valid_move?(index)
-    if position_taken?(index) == true && input_to_index != [0-8]
+    if position_taken?(index) == true && input_to_index(string) != [0-8]
       false
     else 
       true
